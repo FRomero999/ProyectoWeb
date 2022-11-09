deafa03ac0d897490f7685149677bf685c3ef491
Francisco <francisco.romero@cesurformacion.com>
2022-11-09 14:31:36 +0100

    Usos de data- y dialogos

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,19 @@ document.querySelector("aside.menu-offcanvas span").addEventListener(
 function openModal(dialogo){
     document.querySelector(dialogo).classList.toggle("show");
 }
+
+var enlacesDataDialog = document.querySelectorAll("[data-dialog]")
+
+enlacesDataDialog.forEach( function(el){
+    el.addEventListener("click", function(){
+        openModal(this.dataset.dialog)
+    })
+})
+
+let el = document.querySelector("#login [data-dialog]")
+el.addEventListener("click",function(){
+    let nombre = document.querySelector("input[name=nombre]").value
+    document.querySelector("a.menu_usuario").innerText = nombre
+})
+
+
