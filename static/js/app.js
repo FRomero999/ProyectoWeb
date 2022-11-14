64dfe23e18b9d7a32c5517553762b1481919f608
Paco Romero <f.romero.guillen@gmail.com>
2022-11-15 00:01:47 +0100

    Añadido el template para peliculas y obtención de datos con fetch y promises

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,27 @@ el.addEventListener("click",function(){
 })
 
 
+let template = document.querySelector("#plantilla-pelicula")
+let animes = document.querySelector("section#peliculas div")
+
+fetch('https://6372ba75348e947299fbfcdb.mockapi.io/api/v0/animes')
+    .then( (result) => {
+        if(result.ok)
+            return result.json()
+    })
+    .then((output) => {
+        
+        output.forEach( function(pelicula){
+
+            console.log(pelicula)
+
+            let el = template.content.cloneNode(true);
+            el.querySelector("img").setAttribute("src", pelicula.image)
+            el.querySelector("h4").textContent = pelicula.title
+            el.querySelector("p.autor").textContent = pelicula.autor
+            el.querySelector("p.desc").textContent = pelicula.desc
+            animes.appendChild(el)
+
+        })
+        
+}).catch(err => console.error(err))
